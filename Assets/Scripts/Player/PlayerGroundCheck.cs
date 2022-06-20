f4013b655d6d331f3970e7749ab8c2d696a1aa8e
tanya15gupta <66014813+tanya15gupta@users.noreply.github.com>
2022-06-21 00:57:59 +0530

    Added Player Movements

---
+++ Assets/Scripts/Player/PlayerGroundCheck.cs
@@ -0,0 +1,16 @@
+using UnityEngine;
+
+public class PlayerGroundCheck : MonoBehaviour
+{
+    public bool isGrounded;
+
+	private void OnTriggerEnter2D(Collider2D collision)
+	{
+		isGrounded = true;
+	}
+
+	private void OnTriggerExit2D(Collider2D collision)
+	{
+		isGrounded = false;
+	}
+}
